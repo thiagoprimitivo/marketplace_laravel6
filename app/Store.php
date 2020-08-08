06d787db9745fecd903dcfc1b388c819c08bb86d
Thiago Primitivo de Oliveira <thiago.primitivo@gmail.com>
2020-08-08 15:37:54 -0300

    Criadas factories para Store, Product e Category

--- app/Store.php
+++ app/Store.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Store extends Model
 {
+    protected $fillable = ['name', 'description', 'phone', 'mobile_phone', 'slug'];
+
     /**
      * Relação com o usuário - Store belongsTo User
      */
