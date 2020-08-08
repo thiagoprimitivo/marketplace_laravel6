a5fa1fc6c36ec0c2d2ceb93123596dcf70ab5ccc
Thiago Primitivo de Oliveira <thiago.primitivo@gmail.com>
2020-08-08 11:26:14 -0300

    Adicionada relação dos models Store e Product

--- app/Store.php
+++ app/Store.php
@@ -13,4 +13,9 @@ class Store extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
 }
