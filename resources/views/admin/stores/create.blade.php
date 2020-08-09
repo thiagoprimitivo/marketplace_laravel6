6fea95952bf2ee80ec310874991c70975566d784
Thiago Primitivo de Oliveira <thiago.primitivo@gmail.com>
2020-08-09 08:00:50 -0300

    Adicionado o Bootstrap ao projeto

--- resources/views/admin/stores/create.blade.php
+++ resources/views/admin/stores/create.blade.php
@@ -1,30 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
 <h1>Criar loja</h1>
 
 <form action="/admin/stores/store" method="post">
     <input type="hidden" name="_token" value="{{csrf_token()}}">
-    <div>
+    <div class="form-group">
         <label>Nome Loja</label>
-        <input type="text" name="name">
+        <input type="text" name="name" class="form-control">
     </div>
-    <div>
+
+    <div class="form-group">
         <label>Descrição</label>
-        <input type="text" name="description">
+        <input type="text" name="description" class="form-control">
     </div>
-    <div>
+
+    <div class="form-group">
         <label>Telefone</label>
-        <input type="text" name="phone">
+        <input type="text" name="phone" class="form-control">
     </div>
-    <div>
+
+    <div class="form-group">
         <label>Celular/Whatsapp</label>
-        <input type="text" name="mobile_phone">
+        <input type="text" name="mobile_phone" class="form-control">
     </div>
-    <div>
+
+    <div class="form-group">
         <label>Slug</label>
-        <input type="text" name="slug">
+        <input type="text" name="slug" class="form-control">
     </div>
-    <div>
+
+    <div class="form-group">
         <label>Usuário</label>
-        <select name="user">
+        <select name="user" class="form-control">
             @foreach($users as $user)
                 <option value="{{$user->id}}">{{$user->name}}</option>
             @endforeach
@@ -32,6 +40,9 @@
     </div>
 
     <div>
-        <button type="submit">Criar Loja</button>
+        <button type="submit" class="btn btn-lg btn-success">Criar Loja</button>
     </div>
 </form>
+@endsection
+
+
