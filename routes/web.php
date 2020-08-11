cca1d3fbfae66081899f588201b85009eb91bbd7
Thiago Primitivo de Oliveira <thiago.primitivo@gmail.com>
2020-08-11 06:50:05 -0300

    Adicionado autenticação nas rotas do painel administrativo

--- routes/web.php
+++ routes/web.php
@@ -15,20 +15,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::prefix('admin')->name('admin.')->namespace('Admin')->group(function(){
+Route::group(['middleware' => ['auth']], function () {
+    Route::prefix('admin')->name('admin.')->namespace('Admin')->group(function(){
 
-    /*Route::prefix('stores')->name('stores.')->group(function(){
-        Route::get('/', 'StoreController@index')->name('index');
-        Route::get('/create', 'StoreController@create')->name('create');
-        Route::post('/store', 'StoreController@store')->name('store');
-        Route::get('/{store}/edit', 'StoreController@edit')->name('edit');
-        Route::post('/update/{store}', 'StoreController@update')->name('update');
-        Route::get('/destroy/{store}', 'StoreController@destroy')->name('destroy');
-    });*/
+        /*Route::prefix('stores')->name('stores.')->group(function(){
+            Route::get('/', 'StoreController@index')->name('index');
+            Route::get('/create', 'StoreController@create')->name('create');
+            Route::post('/store', 'StoreController@store')->name('store');
+            Route::get('/{store}/edit', 'StoreController@edit')->name('edit');
+            Route::post('/update/{store}', 'StoreController@update')->name('update');
+            Route::get('/destroy/{store}', 'StoreController@destroy')->name('destroy');
+        });*/
 
-    Route::resource('stores', 'StoreController');
-    Route::resource('products', 'ProductController');
+        Route::resource('stores', 'StoreController');
+        Route::resource('products', 'ProductController');
 
+    });
 });
 
 
