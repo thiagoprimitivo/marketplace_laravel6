8e7a9e70c73e29a1a6dab8792c9e5294a2cf6e2b
Thiago Primitivo de Oliveira <thiago.primitivo@gmail.com>
2020-08-12 08:48:24 -0300

    Ajustando a view e controller de lojas para exibir a loja vinculada ao usuário logado

--- app/Http/Controllers/Admin/StoreController.php
+++ app/Http/Controllers/Admin/StoreController.php
@@ -11,19 +11,29 @@ class StoreController extends Controller
 {
     public function index()
     {
-        $stores = \App\Store::paginate(10);
+        $store = auth()->user()->store;
 
-        return view('admin.stores.index', compact('stores'));
+        return view('admin.stores.index', compact('store'));
     }
 
     public function create()
     {
+        if(auth()->user()->store()->count()) {
+            flash('Você já possui uma loja!')->warning();
+            return redirect()->route('admin.stores.index');
+        }
+
         $users = \App\User::all(['id', 'name']);
         return view('admin.stores.create', compact('users'));
     }
 
     public function store(StoreRequest $request)
     {
+        if(auth()->user()->store()->count()) {
+            flash('Você já possui uma loja!')->warning();
+            return redirect()->route('admin.stores.index');
+        }
+
         $data = $request->all();
 
         $user = auth()->user();
--- resources/views/admin/stores/index.blade.php
+++ resources/views/admin/stores/index.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-    <a href="{{route('admin.stores.create')}}" class="btn btn-lg btn-success">Criar Loja</a>
+
+    @if (!$store)
+        <a href="{{route('admin.stores.create')}}" class="btn btn-lg btn-success">Criar Loja</a>
+    @endif
+    
     <table class="table table-striped">
         <thead>
             <tr>
@@ -11,25 +15,21 @@
             </tr>
         </thead>
         <tbody>
-            @foreach($stores as $store)
-                <tr>
-                    <td>{{$store->id}}</td>
-                    <td>{{$store->name}}</td>
-                    <td>
-                        <div class="btn-group">
-                            <a href="{{route('admin.stores.edit', ['store' => $store->id])}}" class="btn btn-sm btn-primary">Editar</a>
-                            <form action="{{route('admin.stores.destroy', ['store' => $store->id])}}" method="post">
-                                @csrf
-                                @method("DELETE")
+            <tr>
+                <td>{{$store->id}}</td>
+                <td>{{$store->name}}</td>
+                <td>
+                    <div class="btn-group">
+                        <a href="{{route('admin.stores.edit', ['store' => $store->id])}}" class="btn btn-sm btn-primary">Editar</a>
+                        <form action="{{route('admin.stores.destroy', ['store' => $store->id])}}" method="post">
+                            @csrf
+                            @method("DELETE")
 
-                                <button type="submit" class="btn btn-sm btn-danger">REMOVER</button>
-                            </form>
-                        </div>
-                    </td>
-                </tr>
-            @endforeach
+                            <button type="submit" class="btn btn-sm btn-danger">REMOVER</button>
+                        </form>
+                    </div>
+                </td>
+            </tr>
         </tbody>
     </table>
-
-    {{$stores->links()}}
 @endsection
