ca63355efade4d666d2dba5d49134931d5a7ff26
Thiago Primitivo de Oliveira <thiago.primitivo@gmail.com>
2020-08-16 15:12:40 -0300

    Criada tela para o checkout

---
+++ resources/views/checkout.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.front')
+
+@section('content')
+    <div class="container">
+        <div class="col-md-6">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>Dados para Pagamento</h2>
+                    <hr>
+                </div>
+            </div>
+            <form action="" method="POST">
+                <div class="row">
+                    <div class="col-md-12 form-group">
+                        <label>Número do Cartão</label>
+                        <input type="text" class="form-control" name="card_number">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4 form-group">
+                        <label>Mês de Expiração</label>
+                        <input type="text" class="form-control" name="card_month">
+                    </div>
+                    <div class="col-md-4 form-group">
+                        <label>Ano de Expiração</label>
+                        <input type="text" class="form-control" name="card_year">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-5 form-group">
+                        <label>Código de Segurança</label>
+                        <input type="text" class="form-control" name="card_cvv">
+                    </div>
+                </div>
+                <button class="btn btn-lg btn-success">Efetuar Pagamento</button>
+            </form>
+        </div>
+    </div>
+@endsection
