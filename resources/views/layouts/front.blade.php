a1998681aa7c7cbc550aa958d4d727df4f02fd34
Thiago Primitivo de Oliveira <thiago.primitivo@gmail.com>
2020-08-22 16:19:51 -0300

    Tela de produtos por categoria

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -32,7 +32,7 @@
 
             @foreach ($categories as $category)
                 <li class="nav-item">
-                    <a class="nav-link" href="#">{{$category->name}}</a>
+                    <a class="nav-link" href="{{ route('category.single', ['slug' => $category->slug]) }}">{{$category->name}}</a>
                 </li>
             @endforeach
         </ul>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@
 
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/product/{slug}', 'HomeController@single')->name('product.single');
+Route::get('/category/{slug}', 'CategoryController@index')->name('category.single');
 
 Route::prefix('cart')->name('cart.')->group(function(){
     Route::get('/', 'CartController@index')->name('index');
