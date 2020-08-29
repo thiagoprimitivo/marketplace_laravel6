daac994d11c88f21f6eca6372f2a1bbc247c314e
Thiago Primitivo de Oliveira <thiago.primitivo@gmail.com>
2020-08-28 22:23:29 -0300

    Notificação via sms

--- app/Notifications/StoreReceiveNewOrder.php
+++ app/Notifications/StoreReceiveNewOrder.php
@@ -5,6 +5,7 @@ namespace App\Notifications;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Messages\NexmoMessage;
 use Illuminate\Notifications\Notification;
 
 class StoreReceiveNewOrder extends Notification
@@ -29,7 +30,7 @@ class StoreReceiveNewOrder extends Notification
      */
     public function via($notifiable)
     {
-        return ['database', 'mail'];
+        return ['database', 'mail', 'nexmo'];
     }
 
     /**
@@ -58,5 +59,13 @@ class StoreReceiveNewOrder extends Notification
         return [
             'message' => 'Você tem um novo pedido solicitado'
         ];
+    }
+
+    public function toNexmo($notifiable)
+    {
+        return (new NexmoMessage)
+                    ->content('Você recebeu um novo pedido em nosso site!')
+                    ->unicode()
+                    ->from('5573991145826');
     }
 }
