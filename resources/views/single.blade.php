91a1af14385a59ea54ad95517f427c9b0b7b8917
Thiago Primitivo de Oliveira <thiago.primitivo@gmail.com>
2020-08-30 21:46:09 -0300

    Efeito de galeria de produtos na single

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -4,11 +4,11 @@
     <div class="row">
         <div class="col-6">
             @if ($product->photos->count())
-                <img src="{{asset('storage/' . $product->photos->first()->image)}}" alt="" class="card-img-top">
+                <img src="{{asset('storage/' . $product->photos->first()->image)}}" alt="" class="card-img-top thumb">
                 <div class="row" style="margin-top: 20px;">
                     @foreach ($product->photos as $photo)
                         <div class="col-4">
-                            <img src="{{asset('storage/' . $photo->image)}}" alt="" class="img-fluid">
+                            <img src="{{asset('storage/' . $photo->image)}}" alt="" class="img-fluid img-small" style="cursor: pointer;">
                         </div>
                     @endforeach
                 </div>
@@ -54,4 +54,17 @@
             {{$product->body}}
         </div>
     </div>
+@endsection
+
+@section('scripts')
+    <script>
+        let thumb = document.querySelector('img.thumb');
+        let imgSmall = document.querySelectorAll('img.img-small');
+
+        imgSmall.forEach(function(el) {
+            el.addEventListener('click', function() {
+                thumb.src = el.src;
+            });
+        });
+    </script>
 @endsection
