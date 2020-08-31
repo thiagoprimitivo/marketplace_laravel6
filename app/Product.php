48ebe242d974caa4cb392175a716024f5a7dd982
Thiago Primitivo de Oliveira <thiago.primitivo@gmail.com>
2020-08-30 22:14:49 -0300

    Criando slugs únicos

--- app/Product.php
+++ app/Product.php
@@ -27,8 +27,16 @@ class Product extends Model
     }
 
     public function setNameAttribute($value) {
+
+        $slug = Str::slug($value);
+        $matchs = $this->uniqueSlug($slug);
+
         $this->attributes['name'] = $value;
-        $this->attributes['slug'] = Str::slug($value);
+        $this->attributes['slug'] = $matchs ? $slug . '-' .$matchs : $slug;
+    }
+
+    public function uniqueSlug($slug) {
+        $matchs = $this->whereRaw("slug REGEXP '^{$slug}(-[0-9]*)?$'")->count();
     }
 
     public function store() {
