c22df4e6e13bfe14757c7e2443b2965753e2138e
Thiago Primitivo de Oliveira <thiago.primitivo@gmail.com>
2020-08-31 13:23:07 -0300

    Corrigindo erro de sessão no checkout

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -14,25 +14,29 @@ class CheckoutController extends Controller
 {
     public function index()
     {
-        //session()->forget('pagseguro_session_code');
+        try {
+            if (!auth()->check()) {
+                return redirect()->route('login');
+            }
 
-        if (!auth()->check()) {
-            return redirect()->route('login');
+            if (!session()->has('cart')) {
+                return redirect()->route('home');
+            }
+
+            $this->makePagSeguroSession();
+
+            $cartItems = array_map(function($product){
+                return $product['amount'] * $product['price'];
+            }, session()->get('cart'));
+
+            $cartItems = array_sum($cartItems);
+
+            return view('checkout', compact('cartItems'));
+
+        } catch (Exception $e) {
+            session()->forget('pagseguro_session_code');
+            redirect()->route('checkout.index');
         }
-
-        if (!session()->has('cart')) {
-            return redirect()->route('home');
-        }
-
-        $this->makePagSeguroSession();
-
-        $cartItems = array_map(function($product){
-            return $product['amount'] * $product['price'];
-        }, session()->get('cart'));
-
-        $cartItems = array_sum($cartItems);
-
-        return view('checkout', compact('cartItems'));
     }
 
     public function process(Request $request)
